e8e7a726732aedea0595afe1afc8bc290e8e4bda
Scott Orwig <scott@orwig.us>
2012-08-15 20:06:48 -0400

    added to installed apps

--- utilisite/settings.py
+++ utilisite/settings.py
@@ -118,6 +118,7 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'south',
     'southtut',
     # Uncomment the next line to enable the admin:
     'django.contrib.admin',
